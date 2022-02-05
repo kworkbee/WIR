d45fbb4752e41360d97514590f05ad7c0fa992cc
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-05 11:46:57 +0900

    feat: New book

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -1,6 +1,7 @@
 export default {
     ongoing: [
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/409/x9791158391409.jpg', title: '오브젝트' },
+        { src: 'http://image.kyobobook.co.kr/images/book/xlarge/383/x9791160508383.jpg', title: '자바와 JUnit을 활용한 실용주의 단위 테스트' },
     ],
     completed: [],
 }
