824ebde257e5cf2d5000c182c2b49459b1d125ad
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-07 23:30:19 +0900

    feat: New book

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -3,6 +3,7 @@ export default {
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/409/x9791158391409.jpg', title: '오브젝트' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/383/x9791160508383.jpg', title: '자바와 JUnit을 활용한 실용주의 단위 테스트' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/441/x9791165210441.jpg', title: '마이크로서비스 패턴' },
+        { src: 'http://image.kyobobook.co.kr/images/book/xlarge/025/x9791162242025.jpg', title: '모던 자바 인 액션' },
     ],
     completed: [],
 }
