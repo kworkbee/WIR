f1582b80898b95f3da36169760c89858f3a7f720
전지원(Jiwon Jeon)/Core플랫폼개발팀/11ST <jiwon_jeon@sk.com>
2022-03-16 23:34:14 +0900

    feat: Add new book, reference

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -4,6 +4,7 @@ export default {
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/383/x9791160508383.jpg', title: '자바와 JUnit을 활용한 실용주의 단위 테스트' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/441/x9791165210441.jpg', title: '마이크로서비스 패턴' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/025/x9791162242025.jpg', title: '모던 자바 인 액션' },
+        { src: 'http://image.kyobobook.co.kr/images/book/xlarge/673/x9791161754673.jpg', title: '스프링으로 하는 마이크로서비스 구축' },
     ],
     completed: [],
 }
--- docs/.vuepress/const.references.js
+++ docs/.vuepress/const.references.js
@@ -1,3 +1,4 @@
 export default [
     { title: "11번가 Spring Cloud 기반 MSA로의 전환 - 지난 1년간의 이야기", src: "https://tv.naver.com/v/3925308", },
+    { title: "Faster Multi-Platform Builds: Dockerfile Cross-Compilation Guide", src: "https://www.docker.com/blog/faster-multi-platform-builds-dockerfile-cross-compilation-guide", },
 ]
