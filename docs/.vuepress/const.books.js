42fb28a5aae0cbc4645160f8837306acce786cc1
G1 <g1.tommy.jeon@gmail.com>
2022-05-22 13:22:59 +0900

    feat: Completed Book && new Reference

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -6,7 +6,8 @@ export default {
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/025/x9791162242025.jpg', title: '모던 자바 인 액션' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/673/x9791161754673.jpg', title: '스프링으로 하는 마이크로서비스 구축' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/283/x9791165216283.jpg', title: '쿠버네티스 완벽 가이드' },
+    ],
+    completed: [
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/217/x9788957360217.jpg', title: '침묵이라는 무기' },
     ],
-    completed: [],
 }
--- docs/.vuepress/const.references.js
+++ docs/.vuepress/const.references.js
@@ -2,4 +2,5 @@ export default [
     { title: "11번가 Spring Cloud 기반 MSA로의 전환 - 지난 1년간의 이야기", src: "https://tv.naver.com/v/3925308", },
     { title: "5 Best Chaos Engineering Tools", src: "https://harness.io/blog/devops/chaos-engineering-tools/" },
     { title: "Faster Multi-Platform Builds: Dockerfile Cross-Compilation Guide", src: "https://www.docker.com/blog/faster-multi-platform-builds-dockerfile-cross-compilation-guide", },
+    { title: "Sealed- secrets 바로 사용하기", src: "https://binux.tistory.com/125" }
 ]
