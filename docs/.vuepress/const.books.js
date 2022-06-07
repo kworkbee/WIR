15b3e027b48e5c74c956f679a4491e164515eb09
G1 <g1.tommy.jeon@gmail.com>
2022-06-07 23:57:51 +0900

    feat: New Book

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -7,6 +7,7 @@ export default {
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/673/x9791161754673.jpg', title: '스프링으로 하는 마이크로서비스 구축' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/283/x9791165216283.jpg', title: '쿠버네티스 완벽 가이드' },
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/472/x9788966262472.jpg', title: '클린 아키텍처' },
+        { src: 'http://image.kyobobook.co.kr/images/book/xlarge/461/x9791160507461.jpg', title: 'RxJava 리액티브 프로그래밍' },
     ],
     completed: [
         { src: 'http://image.kyobobook.co.kr/images/book/xlarge/217/x9788957360217.jpg', title: '침묵이라는 무기' },
