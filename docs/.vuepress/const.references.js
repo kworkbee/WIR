c2a2dc5ca5f8b73ac6f77b1c708000a9e9a8aace
G1 <g1.tommy.jeon@gmail.com>
2022-06-14 00:36:28 +0900

    docs: References

--- docs/.vuepress/const.references.js
+++ docs/.vuepress/const.references.js
@@ -2,5 +2,7 @@ export default [
     { title: "11번가 Spring Cloud 기반 MSA로의 전환 - 지난 1년간의 이야기", src: "https://tv.naver.com/v/3925308", },
     { title: "5 Best Chaos Engineering Tools", src: "https://harness.io/blog/devops/chaos-engineering-tools/" },
     { title: "Faster Multi-Platform Builds: Dockerfile Cross-Compilation Guide", src: "https://www.docker.com/blog/faster-multi-platform-builds-dockerfile-cross-compilation-guide", },
-    { title: "Sealed- secrets 바로 사용하기", src: "https://binux.tistory.com/125" }
+    { title: "Sealed-secrets 바로 사용하기", src: "https://binux.tistory.com/125" },
+    { title: "java 와 spring 을 이용한 헥사고날 아키텍처(hexagonal architecture)", src: "https://sejoung.github.io/2021/06/2021-06-24-hexagonal/#%EC%98%88%EC%A0%9C" },
+    { title: "[IntelliJ IDEA] Java Stream Debugging (스트림 디버깅)", src: "https://binux.tistory.com/145" }, 
 ]
