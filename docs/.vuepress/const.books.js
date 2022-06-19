e15d41baaabddb82ec0b2011ad3260cf000ec94b
전지원 <g1.jeon@kakao.com>
2022-06-19 16:49:37 +0900

    feat: New References

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -7,8 +7,8 @@ export default {
         { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791161754673&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/673/x9791161754673.jpg', title: '스프링으로 하는 마이크로서비스 구축' },
         { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791165216283&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/283/x9791165216283.jpg', title: '쿠버네티스 완벽 가이드' },
         { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788966262472&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/472/x9788966262472.jpg', title: '클린 아키텍처' },
-        { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791160507461&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/461/x9791160507461.jpg', title: 'RxJava 리액티브 프로그래밍' },
-        { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788934944232&orderClick=LAG&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/232/x9788934944232.jpg', title: '불안이라는 중독' },
+        { startdate: '2022. 06. 08.', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791160507461&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/461/x9791160507461.jpg', title: 'RxJava 리액티브 프로그래밍' },
+        { startdate: '2022. 06. 08.', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788934944232&orderClick=LAG&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/232/x9788934944232.jpg', title: '불안이라는 중독' },
     ],
     completed: [
         { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788957360217&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/217/x9788957360217.jpg', title: '침묵이라는 무기' },
