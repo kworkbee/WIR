6b304c6a7466357d937747ee5e9afaab78671aee
G1 <g1.tommy.jeon@gmail.com>
2022-07-17 20:12:30 +0900

    feat: Tech Blogs

---
+++ docs/.vuepress/components/TechBlogs.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="tech-blogs">
+        <ul class="links">
+            <VerticalCard :title="reference.title" :src="reference.src" v-for="(reference, index) in TechBlogList" :key="index" />
+        </ul>
+    </section>
+</template>
+
+<script>
+    import TechBlogList from "../const.tech-blogs"
+    export default {
+        data() {
+            return {
+                TechBlogList,
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ docs/.vuepress/const.tech-blogs.js
@@ -0,0 +1,10 @@
+export default [
+    { title: "11번가 Tech", src: "https://11st-tech.github.io/", },
+    { title: "Kakao Tech", src: "https://tech.kakao.com/", },
+    { title: "NAVER D2", src: "https://d2.naver.com", },
+    { title: "우아한형제들 기술블로그", src: "https://techblog.woowahan.com/", },
+    { title: "HYPERCONNECT 기술블로그", src: "https://hyperconnect.github.io/", },
+    { title: "toss tech", src: "https://toss.tech/", },
+    { title: "당근마켓 팀 블로그", src: "https://medium.com/daangn", },
+    { title: "뱅크샐러드 공식 블로그", src: "https://blog.banksalad.com/", },
+]
