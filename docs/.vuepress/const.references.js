6347cc16df194da5b954ad482488c3e51988a42a
전지원 <g1.jeon@kakao.com>
2022-10-02 18:38:22 +0900

    feat: New Book

--- docs/.vuepress/const.references.js
+++ docs/.vuepress/const.references.js
@@ -1,12 +1,46 @@
 export default [
-    { title: "11번가 Spring Cloud 기반 MSA로의 전환 - 지난 1년간의 이야기", src: "https://tv.naver.com/v/3925308", },
-    { title: "5 Best Chaos Engineering Tools", src: "https://harness.io/blog/devops/chaos-engineering-tools/" },
-    { title: "Faster Multi-Platform Builds: Dockerfile Cross-Compilation Guide", src: "https://www.docker.com/blog/faster-multi-platform-builds-dockerfile-cross-compilation-guide", },
-    { title: "Sealed-secrets 바로 사용하기", src: "https://binux.tistory.com/125" },
-    { title: "java 와 spring 을 이용한 헥사고날 아키텍처(hexagonal architecture)", src: "https://sejoung.github.io/2021/06/2021-06-24-hexagonal/#%EC%98%88%EC%A0%9C" },
-    { title: "[IntelliJ IDEA] Java Stream Debugging (스트림 디버깅)", src: "https://binux.tistory.com/145" }, 
-    { title: "RBAC와 ABAC 의 특징 비교: 정의 및 사용 사례", src: "https://www.okta.com/kr/identity-101/role-based-access-control-vs-attribute-based-access-control/"},
-    { title: "상태 일병 구하기 - State Machine", src: "https://dealicious-inc.github.io/2021/04/12/state-machine.html" },
-    { title: "Armeria로 Reactive Streams와 놀자! - 1", src: "https://engineering.linecorp.com/ko/blog/reactive-streams-with-armeria-1/" },
-    { title: "Armeria로 Reactive Streams와 놀자! - 2", src: "https://engineering.linecorp.com/ko/blog/reactive-streams-with-armeria-2/" },
-]
+  {
+    title: "11번가 Spring Cloud 기반 MSA로의 전환 - 지난 1년간의 이야기",
+    src: "https://tv.naver.com/v/3925308",
+  },
+  {
+    title: "5 Best Chaos Engineering Tools",
+    src: "https://harness.io/blog/devops/chaos-engineering-tools/",
+  },
+  {
+    title: "Faster Multi-Platform Builds: Dockerfile Cross-Compilation Guide",
+    src: "https://www.docker.com/blog/faster-multi-platform-builds-dockerfile-cross-compilation-guide",
+  },
+  {
+    title: "Sealed-secrets 바로 사용하기",
+    src: "https://binux.tistory.com/125",
+  },
+  {
+    title: "java 와 spring 을 이용한 헥사고날 아키텍처(hexagonal architecture)",
+    src: "https://sejoung.github.io/2021/06/2021-06-24-hexagonal/#%EC%98%88%EC%A0%9C",
+  },
+  {
+    title: "[IntelliJ IDEA] Java Stream Debugging (스트림 디버깅)",
+    src: "https://binux.tistory.com/145",
+  },
+  {
+    title: "RBAC와 ABAC 의 특징 비교: 정의 및 사용 사례",
+    src: "https://www.okta.com/kr/identity-101/role-based-access-control-vs-attribute-based-access-control/",
+  },
+  {
+    title: "상태 일병 구하기 - State Machine",
+    src: "https://dealicious-inc.github.io/2021/04/12/state-machine.html",
+  },
+  {
+    title: "Armeria로 Reactive Streams와 놀자! - 1",
+    src: "https://engineering.linecorp.com/ko/blog/reactive-streams-with-armeria-1/",
+  },
+  {
+    title: "Armeria로 Reactive Streams와 놀자! - 2",
+    src: "https://engineering.linecorp.com/ko/blog/reactive-streams-with-armeria-2/",
+  },
+  {
+    title: "Writing Better Code with Reactor 3 - Part 3",
+    src: "https://codeyoucanuse.com/2020/06/24/writing-better-code-with-reactor-3-part-3.html",
+  },
+];
