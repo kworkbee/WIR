361cbb96baeb9d4256e91572a93c8ca322b83d62
G1 <g1.tommy.jeon@gmail.com>
2022-10-24 23:54:18 +0900

    feat: New Book

--- docs/.vuepress/const.books.js
+++ docs/.vuepress/const.books.js
@@ -9,6 +9,7 @@ export default {
         { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788966262472&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/472/x9788966262472.jpg', title: '클린 아키텍처' },
         { startdate: '2022. 06. 08.', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791160507461&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/461/x9791160507461.jpg', title: 'RxJava 리액티브 프로그래밍' },
         { startdate: '2022. 06. 08.', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788934944232&orderClick=LAG&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/232/x9788934944232.jpg', title: '불안이라는 중독' },
+        { startdate: '2022. 10. 18.', src: 'https://product.kyobobook.co.kr/detail/S000061352347', thumbnail: 'https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791162245620.jpg', title: '구글 엔지니어는 이렇게 일한다' },
     ],
     completed: [
         { startdate: '', src: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9788957360217&orderClick=LEa&Kc=', thumbnail: 'http://image.kyobobook.co.kr/images/book/xlarge/217/x9788957360217.jpg', title: '침묵이라는 무기' },
